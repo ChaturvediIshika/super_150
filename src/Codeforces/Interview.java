84e1ec1c17e0671ed0f7d3cb5b977a5e4a7cb5ca
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2023-03-20 19:15:02 +0530

    commit

--- src/Codeforces/Interview.java
+++ src/Codeforces/Interview.java
@@ -6,38 +6,38 @@ public class Interview {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
-        while(t-->0){
-            int n=sc.nextInt();
-            int a[]=new int[n];
-            long arr[]=new long[n+1];
-            long sum=0;
-            for (int i=0;i<n;i++){
-                a[i]=sc.nextInt();
-                sum+=a[i];
-                arr[i]=sum;
-            }
-            int idx=0;
-            int l=0,h=n-1;
-            while (l<=h){
-                int mid=l+(h-l)/2;
-                if(l==0)
-                    sum=arr[mid];
-                else{
-                    sum=arr[mid]-arr[l-1];
-                }
-                System.out.print("?"+(mid-l+1)+" ");
-                for (int i=l;i<=mid;i++)
-                    System.out.print((i+1)+" ");
-                System.out.println();
-                long x=sc.nextLong();
-                if(x!=sum){
-                    idx=mid+1;
-                    h=mid-1;
-                }
-                else
-                    l=mid+1;
-            }
-            System.out.println("! "+idx);
-        }
+//        while(t-->0){
+//            int n=sc.nextInt();
+//            int a[]=new int[n];
+//            long arr[]=new long[n+1];
+//            long sum=0;
+//            for (int i=0;i<n;i++){
+//                a[i]=sc.nextInt();
+//                sum+=a[i];
+//                arr[i]=sum;
+//            }
+////            int idx=0;
+////            int l=0,h=n-1;
+////            while (l<=h){
+////                int mid=l+(h-l)/2;
+////                if(l==0)
+////                    sum=arr[mid];
+////                else{
+////                    sum=arr[mid]-arr[l-1];
+////                }
+//////                System.out.print("?"+(mid-l+1)+" ");
+////                for (int i=l;i<=mid;i++)
+////                    System.out.print((i+1)+" ");
+////                System.out.println();
+////                long x=sc.nextLong();
+////                if(x!=sum){
+////                    idx=mid+1;
+////                    h=mid-1;
+////                }
+////                else
+////                    l=mid+1;
+////            }
+//            System.out.println("! "+idx);
+////        }
     }
 }
