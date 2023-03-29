a4cba4bac2e3ae986e07ea2bcfb7611e123f7cc4
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2023-03-29 09:15:25 +0530

    commit

---
+++ src/Codeforces_Daily_Question/Domino_piling.java
@@ -0,0 +1,12 @@
+package Codeforces_Daily_Question;
+
+import java.util.Scanner;
+
+public class Domino_piling {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int m= sc.nextInt();
+        System.out.println((n*m)/2);
+    }
+}
