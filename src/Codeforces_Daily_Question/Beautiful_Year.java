8e6b5c16e18b1354a3fd632845ad846fcac95b72
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2023-04-02 16:37:05 +0530

    commit

---
+++ src/Codeforces_Daily_Question/Beautiful_Year.java
@@ -0,0 +1,20 @@
+package Codeforces_Daily_Question;
+
+import java.util.Scanner;
+
+public class Beautiful_Year {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        while (true){
+            y+=1;
+            int a=y/1000;
+            int b=y/100%10;
+            int c=y/10%10;
+            int d=y%10;
+            if(a!=b && a!=c && a!=d && b!=c && b!=d && c!=d)
+                break;
+        }
+        System.out.println(y);
+    }
+}
