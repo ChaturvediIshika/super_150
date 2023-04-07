588793e3b02bdc85aaf17ada67cdcc3c902e9d2e
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2023-04-07 18:46:30 +0530

    commit

---
+++ src/Codeforces_Daily_Question/Calculating_Function.java
@@ -0,0 +1,14 @@
+package Codeforces_Daily_Question;
+
+import java.util.Scanner;
+
+public class Calculating_Function {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+        if(n%2==0)
+            System.out.println(n/2);
+        else
+            System.out.println(((n+1)/2)*(-1));
+    }
+}
