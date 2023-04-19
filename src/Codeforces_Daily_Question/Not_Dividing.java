90e9b4296fae2a6fed6bff69bbfbdaa9f3780562
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2023-04-19 09:24:02 +0530

    commit

---
+++ src/Codeforces_Daily_Question/Not_Dividing.java
@@ -0,0 +1,26 @@
+package Codeforces_Daily_Question;
+
+import java.util.Scanner;
+
+public class Not_Dividing {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t= sc.nextInt();
+        while (t-->0){
+            int n= sc.nextInt();
+            int a[]=new int[n];
+            for (int i=0;i<n;i++){
+                a[i]= sc.nextInt();
+                if(a[i]==1)
+                    a[i]++;
+            }
+            for (int i=0;i<n-1;i++){
+                if(a[i+1]%a[i]==0)
+                    a[i+1]++;
+            }
+            for (int i=0;i<n;i++)
+                System.out.print(a[i]+" ");
+            System.out.println();
+        }
+    }
+}
