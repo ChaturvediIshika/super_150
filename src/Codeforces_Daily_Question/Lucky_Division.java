fa01de3d14ec9e1c86490ac151c9f302da74852a
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2023-06-19 14:49:36 +0530

    commit

---
+++ src/Codeforces_Daily_Question/Lucky_Division.java
@@ -0,0 +1,14 @@
+package Codeforces_Daily_Question;
+
+import java.util.Scanner;
+
+public class Lucky_Division {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%4==0 || n%7==0 || n%47==0 || n%74==0 || n%477==0||n%447==0 || n%474==0 || n%744==0 || n%747==0 || n%774==0)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
