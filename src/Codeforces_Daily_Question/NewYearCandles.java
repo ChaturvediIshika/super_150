5e3052b5ae313cadd96c0b8598ea7202d61054e1
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2023-09-06 11:41:57 +0530

    commit

---
+++ src/Codeforces_Daily_Question/NewYearCandles.java
@@ -0,0 +1,12 @@
+package Codeforces_Daily_Question;
+
+import java.util.Scanner;
+
+public class NewYearCandles {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+        System.out.println(a+(a-1)/(b-1));
+    }
+}
