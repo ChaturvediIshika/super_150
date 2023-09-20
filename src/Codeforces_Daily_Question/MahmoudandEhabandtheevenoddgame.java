78ea9d289fac8c5c2c0aa3248126752d3886059d
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2023-09-20 13:09:52 +0530

    commit

---
+++ src/Codeforces_Daily_Question/MahmoudandEhabandtheevenoddgame.java
@@ -0,0 +1,14 @@
+package Codeforces_Daily_Question;
+
+import java.util.Scanner;
+
+public class MahmoudandEhabandtheevenoddgame {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        if(n%2!=0)
+            System.out.println("Ehab");
+        else
+            System.out.println("Mahmoud");
+    }
+}
