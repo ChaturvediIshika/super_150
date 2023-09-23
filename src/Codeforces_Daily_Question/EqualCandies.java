216ccf73ae0515359d2ec9841ca9e6f6523eba06
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2023-09-23 12:07:22 +0530

    commit

---
+++ src/Codeforces_Daily_Question/EqualCandies.java
@@ -0,0 +1,22 @@
+package Codeforces_Daily_Question;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EqualCandies {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t= sc.nextInt();
+        while (t-->0){
+            int n= sc.nextInt();
+            int a[]=new int[n];
+            for (int i=0;i<n;i++)
+                a[i]=sc.nextInt();
+            int sum=0;
+            Arrays.sort(a);
+            for(int i=1;i<n;i++)
+                sum+=a[i]-a[0];
+            System.out.println(sum);
+        }
+    }
+}
