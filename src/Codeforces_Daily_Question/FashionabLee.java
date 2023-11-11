74bb968861cc555d9b159244db5ae27473eb1bb2
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2023-11-11 12:50:35 +0530

    commit

---
+++ src/Codeforces_Daily_Question/FashionabLee.java
@@ -0,0 +1,17 @@
+package Codeforces_Daily_Question;
+
+import java.util.Scanner;
+
+public class FashionabLee {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            if(n%4==0)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+}
