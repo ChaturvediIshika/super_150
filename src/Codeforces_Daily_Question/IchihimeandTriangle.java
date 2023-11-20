11f629944d7f0781167d4d1734dfafc2e92f0de7
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2023-11-20 12:25:06 +0530

    commit

---
+++ src/Codeforces_Daily_Question/IchihimeandTriangle.java
@@ -0,0 +1,17 @@
+package Codeforces_Daily_Question;
+
+import java.util.Scanner;
+
+public class IchihimeandTriangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t= sc.nextInt();
+        while (t-->0){
+            int a=sc.nextInt();
+            int b= sc.nextInt();
+            int c= sc.nextInt();
+            int d= sc.nextInt();
+            System.out.println(b+" "+c+" "+c);
+        }
+    }
+}
