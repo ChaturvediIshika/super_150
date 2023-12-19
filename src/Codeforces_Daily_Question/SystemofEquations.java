5877141408120bcb91a1e8fc3647c8a21b37eefe
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2023-12-19 12:02:39 +0530

    commit

---
+++ src/Codeforces_Daily_Question/SystemofEquations.java
@@ -0,0 +1,19 @@
+package Codeforces_Daily_Question;
+
+import java.util.Scanner;
+
+public class SystemofEquations {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int m= sc.nextInt();
+        int c=0;
+        for(int i=0;i<=1000;i++){
+            for(int j=0;j<=1000;j++){
+                if(i*i+j==n && i+j*j==m)
+                    c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
