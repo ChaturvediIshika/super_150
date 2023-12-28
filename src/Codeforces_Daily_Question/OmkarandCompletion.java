c4e91b68d8c6fe6f40a4fa8c36e367f2e9b916f1
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2023-12-28 15:52:02 +0530

    commit

---
+++ src/Codeforces_Daily_Question/OmkarandCompletion.java
@@ -0,0 +1,17 @@
+package Codeforces_Daily_Question;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class OmkarandCompletion {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t= sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            while (n-->0)
+                System.out.print("1 ");
+            System.out.println();
+        }
+    }
+}
