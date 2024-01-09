da9f0dd888f0e5bfa1ab5ad89a608e44a289b7ed
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2024-01-09 13:26:54 +0530

    commit

---
+++ src/Codeforces_Daily_Question/FillingDiamonds.java
@@ -0,0 +1,14 @@
+package Codeforces_Daily_Question;
+
+import java.util.Scanner;
+
+public class FillingDiamonds {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t= sc.nextInt();
+        while (t-->0){
+            int n= sc.nextInt();
+            System.out.println(n);
+        }
+    }
+}
