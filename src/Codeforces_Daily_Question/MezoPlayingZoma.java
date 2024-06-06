9f7dd042503d6c4f863c55e5a26d02a1d44fd4c3
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2024-06-06 15:53:01 +0530

    commit

---
+++ src/Codeforces_Daily_Question/MezoPlayingZoma.java
@@ -0,0 +1,12 @@
+package Codeforces_Daily_Question;
+
+import java.util.Scanner;
+
+public class MezoPlayingZoma {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        String s= sc.next();
+        System.out.println(n+1);
+    }
+}
