22a6e2323408541bfb83d70f2e8155c152b0e18b
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2024-06-13 11:44:44 +0530

    commit

---
+++ src/Codeforces_Daily_Question/MaximumMultipleSum.java
@@ -0,0 +1,17 @@
+package Codeforces_Daily_Question;
+
+import java.util.Scanner;
+
+public class MaximumMultipleSum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t= sc.nextInt();
+        while (t-->0){
+            int n= sc.nextInt();
+            if(n==3)
+                System.out.println(3);
+            else
+                System.out.println(2);
+        }
+    }
+}
