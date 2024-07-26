4757967c154bd877605731613ca8138f70a4d2db
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2024-07-26 08:30:11 +0530

    commit

---
+++ src/Codeforces_Daily_Question/Coins.java
@@ -0,0 +1,18 @@
+package Codeforces_Daily_Question;
+
+import java.util.Scanner;
+
+public class Coins {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            long n=sc.nextLong();
+            long k= sc.nextLong();
+            if(n%2==1&&k%2==0)
+                System.out.println("NO");
+            else
+                System.out.println("YES");
+        }
+    }
+}
