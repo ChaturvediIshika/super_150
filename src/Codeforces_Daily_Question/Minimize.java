30bd06ee806b33bf820ba9bc0ecf9413aa1cbc39
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2024-09-04 10:22:36 +0530

    commit

---
+++ src/Codeforces_Daily_Question/Minimize.java
@@ -0,0 +1,15 @@
+package Codeforces_Daily_Question;
+
+import java.util.Scanner;
+
+public class Minimize {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t= sc.nextInt();
+        while (t-->0){
+            int a= sc.nextInt();
+            int b= sc.nextInt();
+            System.out.println(b-a);
+        }
+    }
+}
